584038898210c4c8c1e57293f92eb9b474ffb383
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2025-01-01 07:13:34 +0530

    calculating the age

---
+++ day1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+birth_year = int(input("What year were you born? "))
+age = 2024 - birth_year
+print("You are " + str(age) + " years old.")
