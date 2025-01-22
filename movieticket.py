ecccd0b5db243896822d1e6a8382846a40be9bdd
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2025-01-22 06:54:15 +0530

    Update movieticket.py

--- movieticket.py
+++ movieticket.py
@@ -1,56 +1,87 @@
 import datetime
+
 while True:
- print('Welcome to movie ticket booking system')
- movie=int(input('Enter the movie name:\n1)Intestellar\n2)Inception\n3)The Dark Knight\n4)The Prestige\n5)Dunkirk\n'))
- ticket=int(input("Enter the number of tickets you want to book\n"))
- type=int(input('Enter the type of ticket you want to book\n1)Platinum\n2)normal\n'))
- if type==1:
-     price=200
- else:
-     price=100
- time=int(input('Enter the show time:\n 1)10:00am\n2)2:00pm\n3)6:00pm\n4)10:00pm\n'))
- date_str = input('Enter the date of the show (YYYY-MM-DD):\n')
- try:
-     show_date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
- except ValueError:
-     print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
-     continue
- if ticket > 5:
-      price =ticket*price*0.9
- else:
-        price =ticket*price
+    print('Welcome to the Movie Ticket Booking System')
 
- if movie == 1:
-        movie = 'Interstellar'
- elif movie == 2:
-        movie = 'Inception'
- elif movie == 3:
-        movie = 'The Dark Knight'
- elif movie == 4:
-        movie = 'The Prestige'
- elif movie == 5:
-        movie = 'Dunkirk'
- else:
-        print('Invalid movie selection')
-        continue
+  
+    print('Select a movie:')
+    print('1) Interstellar')
+    print('2) Inception')
+    print('3) The Dark Knight')
+    print('4) The Prestige')
+    print('5) Dunkirk')
+    movie_choice = int(input('Enter your choice (1-5): '))
     
-   
- if time == 1:
-        time = '10:00am'
- elif time == 2:
-        time = '2:00pm'
- elif time == 3:
-        time = '6:00pm'
- elif time == 4:
-        time = '10:00pm'
- else:
-        print('Invalid time selection')
+    if movie_choice == 1:
+        movie_name = 'Interstellar'
+    elif movie_choice == 2:
+        movie_name = 'Inception'
+    elif movie_choice == 3:
+        movie_name = 'The Dark Knight'
+    elif movie_choice == 4:
+        movie_name = 'The Prestige'
+    elif movie_choice == 5:
+        movie_name = 'Dunkirk'
+    else:
+        print('Invalid movie selection. Please try again.')
         continue
+
+   
+    ticket_count = int(input("Enter the number of tickets you want to book: "))
+
  
- print(f'The total price of the tickets is {price}\nyour show is {movie}\ntime is {time}\ndate is {show_date.strftime("%Y-%m-%d")}')
- break
+    print('Select ticket type:')
+    print('1) Platinum (₹200)')
+    print('2) Normal (₹100)')
+    ticket_type = int(input('Enter your choice (1 or 2): '))
+    
+    if ticket_type == 1:
+        price_per_ticket = 200
+    elif ticket_type == 2:
+        price_per_ticket = 100
+    else:
+        print('Invalid ticket type. Please try again.')
+        continue
 
+   
+    print('Select show time:')
+    print('1) 10:00am')
+    print('2) 2:00pm')
+    print('3) 6:00pm')
+    print('4) 10:00pm')
+    show_time_choice = int(input('Enter your choice (1-4): '))
+    
+    if show_time_choice == 1:
+        show_time = '10:00am'
+    elif show_time_choice == 2:
+        show_time = '2:00pm'
+    elif show_time_choice == 3:
+        show_time = '6:00pm'
+    elif show_time_choice == 4:
+        show_time = '10:00pm'
+    else:
+        print('Invalid time selection. Please try again.')
+        continue
 
+  
+    date_str = input('Enter the date of the show (YYYY-MM-DD): ')
+    
+    try:
+        show_date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+        continue
 
+    total_price = ticket_count * price_per_ticket
+    if ticket_count > 5:
+        total_price *= 0.9
 
-
+    print('\nYOU HAVE SUCCESSFULLY BOOKED YOUR TICKET\nBooking Details:')
+    print(f'Movie: {movie_name}')
+    print(f'Tickets: {ticket_count}')
+    print(f'Ticket Type: {"Platinum" if ticket_type == 1 else "Normal"}')
+    print(f'Show Time: {show_time}')
+    print(f'Show Date: {show_date.strftime("%Y-%m-%d")}')
+    print(f'Total Price: ₹{total_price:.2f}')
+    
+    break
